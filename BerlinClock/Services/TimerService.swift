af08b554af3adca8975a66bf8574a5443aecc6c7
2022-DEV2-030 <2022-DEV2-030@example.com>
2023-01-25 09:50:10 +0100

    fix timer service

--- BerlinClock/Services/TimerService.swift
+++ BerlinClock/Services/TimerService.swift
@@ -4,9 +4,20 @@ final class TimerService {
 
     @Published var timePublisher = Date()
 
+    // MARK: - Init
+
     init() {
+        publish()
+    }
+}
+
+// MARK: - Private methods
+
+private extension TimerService {
+    func publish() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
             self?.timePublisher = Date()
+            self?.publish()
         }
     }
 }
