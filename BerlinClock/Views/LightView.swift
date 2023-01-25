bdfde8f89ba71e100836b81abe3cfad6e169e7af
2022-DEV2-030 <2022-DEV2-030@example.com>
2023-01-25 08:36:53 +0100

    add light view

---
+++ BerlinClock/Views/LightView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct LightView: View {
+
+    @Binding var light: Light
+
+    var body: some View {
+        Circle()
+            .strokeBorder(.black, lineWidth: 2)
+            .background(Circle().fill(lightColor(light: light)))
+            .frame(width: 30, height: 30)
+    }
+}
+
+// MARK: - Private methods
+
+private extension LightView {
+
+    func lightColor(light: Light) -> Color {
+        switch light {
+        case .red:
+            return .red
+        case .yellow:
+            return .yellow
+        default:
+            return .clear
+        }
+    }
+}
+
+// MARK: - Preview
+
+struct LightView_Previews: PreviewProvider {
+
+    @State static var light = Light.off
+
+    static var previews: some View {
+        LightView(light: $light)
+    }
+}
